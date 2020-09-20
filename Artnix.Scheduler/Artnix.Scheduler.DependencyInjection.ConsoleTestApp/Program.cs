75366e33a933009077e567156421e8994d8aa8f5
arttonoyan <arttonoyan@gmail.com>
2020-09-20 05:10:41 +0400

    Add ConsoleTestApp for DependencyInjection project

---
+++ Artnix.Scheduler/Artnix.Scheduler.DependencyInjection.ConsoleTestApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Artnix.Scheduler.DependencyInjection.ConsoleTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var services = new ServiceCollection();
+            services.AddScheduler(cfg =>
+            {
+                cfg.CreaJobService<MyJobRed>(b => b.ToRunOnceIn(2).Seconds().AtStartTime());
+                cfg.CreaJobService<MyJobGreen>(b => b.ToRunOnceIn(3).Seconds().AtStartTime());
+            });
+
+            var provider = services.BuildServiceProvider();
+            var scheduler = provider.GetService<IScheduler>();
+            scheduler.Start();
+
+            Console.WriteLine("Started");
+            Console.ReadLine();
+        }
+    }
+
+    public class MyJobRed : IJob
+    {
+        public void Execute()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(DateTime.Now.ToString("yyyy MM dd HH:mm:ss"));
+            Console.ResetColor();
+        }
+    }
+
+    public class MyJobGreen : IJob
+    {
+        public void Execute()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(DateTime.Now.ToString("yyyy MM dd HH:mm:ss"));
+            Console.ResetColor();
+        }
+    }
+}
