8693aa6c9287f842ebadf507c132b889ea1e912e
arttonoyan <arttonoyan@gmail.com>
2020-09-20 06:30:30 +0400

    Change namespace

--- Artnix.Scheduler/Artnix.Scheduler.DependencyInjection/ServiceCollectionExtensions.cs
+++ Artnix.Scheduler/Artnix.Scheduler.DependencyInjection/ServiceCollectionExtensions.cs
@@ -2,7 +2,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using System;
 
-namespace Artnix.Scheduler.DependencyInjection
+namespace Artnix.Scheduler
 {
     public static class ServiceCollectionExtensions
     {
